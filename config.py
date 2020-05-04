b22be63a3e72464c455a507beed8f2bfc4864ede
William Hawley <whawley@gaherty-mac.ldeo.columbia.edu>
2020-05-04 14:56:32 -0400

    Change 01 to use on Gaherty-mac; add git-ignore

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-## dirs and files
-BaseDir = '/Users/wbhawley/Research/Seismology/Blanco_SurfaceWave/'
+#  dirs and files
+BaseDir = '/Users/whawley/Research/github/Blanco-SW/'
 ConfigDir = BaseDir+'config/'
 DataDir = BaseDir+'data/'
 EventsFileName = ConfigDir+'EventsTest.txt'
@@ -10,39 +10,39 @@ XStafn = ConfigDir+'X9_stations_X.txt'
 DStafn = ConfigDir+'X9_stations_D.txt'
 Stafn = ConfigDir+'X9_stations.txt'
 
-## Event download
+#  Event download
 minMag = 6.5
 webservice = "IRIS"
-network = "X9" # X9 = Blanco
-isCMT_params = 1 # use GCMT parameters for SAC header; 0 = use IRIS
-isCentroid = 1 # if isCMT_params = 1, use centroid; 0 = epicentral
+network = "X9"  # X9 = Blanco
+isCMT_params = 1  # use GCMT parameters for SAC header; 0 = use IRIS
+isCentroid = 1  # if isCMT_params = 1, use centroid; 0 = epicentral
 
 tstart = '2012-11-16T00:00:00'
 tend = '2012-11-16T23:00:00'
 
-## Stations to download
+# Stations to download
 # need one for low-pass filtered stations
 inFile = open(XStafn)
 XStaList = []
 for line in inFile:
     XStaList.append(line.rstrip('\n'))
-XChanList = ['LHZ','LH1','LH2','BXH']
+XChanList = ['LHZ', 'LH1', 'LH2', 'BXH']
 
 # and for regular stations...
 inFile = open(DStafn)
 DStaList = []
 for line in inFile:
     DStaList.append(line.rstrip('\n'))
-DChanList = ['LHZ','LH1','LH2','BDH']
+DChanList = ['LHZ', 'LH1', 'LH2', 'BDH']
 
 # and for all stations...
 inFile = open(Stafn)
 StaList = []
 for line in inFile:
     StaList.append(line.rstrip('\n'))
-ChanList = ['LHZ','LH1','LH2','BDH']
-    
+ChanList = ['LHZ', 'LH1', 'LH2', 'BDH']
+
 # trace info
-isDownsamp = 1 # downsample option
-srNew = 1 # new sample rate, samples/sec
-trLen = 6000 # len of traces (sec)
+isDownsamp = 1  # downsample option
+srNew = 1  # new sample rate, samples/sec
+trLen = 6000  # len of traces (sec)
